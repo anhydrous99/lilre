a717ebd8ebc77aceb627d86b851363015ddf16c7
Armando Herrera <anhydrous99@gmail.com>
2023-10-21 20:37:50 -0500

    Created Form

--- lilre-site/src/App.tsx
+++ lilre-site/src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Button, Alert, Container, Navbar, Form as BForm } from 'react-bootstrap';
+import { object, string } from 'yup';
 import './App.css';
+
+const urlPostSchema = object({
+  url: string().required().max(25000).min(6).url().ensure()
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  <div>
+    <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand>LilRe - Url Shortener</Navbar.Brand>
+      </Container>
+    </Navbar>
+
+    <Container className='flex-fill'>
+      <div className='jumbotron col-md-10 border p-3 m-5'>
+        <Formik
+          initialValues={{ url: '' }}
+          validationSchema={urlPostSchema}
+          onSubmit={( values, { setSubmitting } ) => {
+            console.log(values)
+            setSubmitting(false);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {({ errors, touched, isSubmitting }) => (
+            <Form>
+              <BForm.Group className="mb-3">
+                <BForm.Label>Url</BForm.Label>
+                <Field type="text" name="url" className="form-control" />
+                {errors.url && touched.url ? (
+                  <Alert key='info' variant='info' className="mt-2">{ errors.url }</Alert>
+                ) : null}
+              </BForm.Group>
+              <Button variant="primary" type="submit" disabled={ isSubmitting }>Submit</Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </Container>
+  </div>
   );
 }
 
